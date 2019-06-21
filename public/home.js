31d7e881b2049c4288e2f5f07ce54b848fec67fa
ahlamkadour1992 <ahlamkadour2016@gmail.com>
2019-06-21 16:35:29 +0300

    Tech Test

--- public/home.js
+++ public/home.js
@@ -1,11 +1,15 @@
 document.getElementById('submitBtn').addEventListener('click', function(e) {
   e.preventDefault();
   var name = document.getElementById('name').value;
-
+console.log('name is:', name);
   // Use function sendInfo to POST information to server
-  sendInfo('/user/' + name, 'GET', name, function(err, success) {
+  sendInfo('/user/' + name, 'POST', name, function(err, success) {
       // Some things are missing...
-      window.location = success.redirect; // You will need this line somewhere ;)
-    }
+    if (err) {
+    console.log('This is error in sendInfo function', err);
+  } else {
+    console.log('success', success);
+    window.location = success.redirect; // You will need this line somewhere ;)
+  }
   });
-});
+  });
--- src/database/queries/animal_queries.js
+++ src/database/queries/animal_queries.js
@@ -1,27 +1,33 @@
 const dbConnect = require('../db_connect');
 
 const displayAnimals = (cb) => {
-  dbConnect.query(`SELECT name FROM animals;`, (e, res) => {
+  dbConnect.query(`SELECT animal_name FROM animals;`, (e, res) => {
     // humm...
-    return cb(res.rows);
+    if (e) return cb(e);
+    console.log('This is the res.rows', res.rows);
+    return cb(null, res.rows);
   });
-}
+};
 
 const fetchSingleAnimal = (animal_name, cb) => {
   // humm...
-  dbConnect.query(`SELECT * FROM animals WHERE animal_name = {$1}`, [animal_name], (e, res) => {
-
-    return cb(e, res);
-  })
-}
+  dbConnect.query(`SELECT * FROM animals WHERE animal_name = ($1)`, [animal_name], (e, res) => {
+    if (e) return cb(e);
+    console.log('This is the res.rows', res.rows);
+    return cb(null, res.rows);
+  });
+};
 
 const insertAnimal = (data, cb) => {
     dbConnect.query(`INSERT INTO animals(animal_name, img_link, habitat, origin, facts, myths) VALUES($1, $2, $3, $4, $5, $6)`,
-  [data], (e, success) => {
+  [data], (e, res) => {
     // hum...
-    return success;
-  })
-}
+    if (e) return cb(e);
+    // return success;
+    console.log('animal been added to animals table');
+    cb(null, true);
+  });
+};
 
 module.exports = {
   displayAnimals,
